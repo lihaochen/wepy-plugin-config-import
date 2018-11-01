c77b90fc5540d2d96d8e293d0fba5708adff5cab
Bryan Lee <li.hao2@mydreamplus.com>
2018-11-01 11:33:01 +0800

    bug fix for version 0.0.3

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import fs from 'fs-extra'
 
 /**
  * 引入组件并替换 usingComponents 中的值
- * @param {} components 组件列表
+ * @param components 组件列表
  */
 function ImportComponentsAndReplacePath(components) {
     Object.keys(components).map(name => {
         // 如果 components 以 ~ 开头，则解析为 node_modules 中的包，此时对该组件进行copy并修改路径
         if (/^\~/.test(components[name])) {
             importComponents.call(this, components[name])
-            replaceImportPath.call(this, components)
+            replaceImportPath.call(this, components, name)
         }
     })
 }
@@ -18,7 +18,7 @@ function ImportComponentsAndReplacePath(components) {
 /**
  * replace path
  */
-function replaceImportPath(components) {
+function replaceImportPath(components, name) {
     let targetCompPath = path.resolve(process.cwd(), components[name].replace('~', 'dist/npm/'))
     targetCompPath = path.resolve(targetCompPath, 'index')
     components[name] = path.relative(path.dirname(this.file), targetCompPath)
@@ -37,13 +37,16 @@ function importComponents(component) {
 
 export default class {
     constructor(options) {
-        return function doSomething() {
-            // this is op
+        return function() {
             if (this.type === 'config') {
                 try {
                     let config = JSON.parse(this.code)
                     if (config.usingComponents) {
-                        ImportComponentsAndReplacePath.call(this, config.usingComponents)
+                        try {
+                            ImportComponentsAndReplacePath.call(this, config.usingComponents)
+                        } catch (e) {
+                            console.error(e)
+                        }
                         this.code = JSON.stringify(config)
                         this.done(this)
                     } else {
